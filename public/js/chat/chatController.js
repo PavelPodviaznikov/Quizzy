1cfe8f90729eae922be9fef024c39ced5d040397
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2015-12-11 10:18:32 +0200

    feat(chat) check the message, stay at the bottom

--- public/js/chat/chatController.js
+++ public/js/chat/chatController.js
@@ -4,21 +4,35 @@
     angular.module('chat')
         .controller('ChatController', chatController);
 
-        chatController.$inject = ['$scope', 'socketService'];
+        chatController.$inject = ['$scope', '$element', '$timeout', 'socketService'];
 
-        function chatController ($scope, socketService) {
+        function chatController ($scope, $element, $timeout, socketService) {
 
             $scope.message = '';
             $scope.messages = [];
+            $scope.sendMessage = sendMessage;
 
-            socketService.listen('new message', function(message) {
+            socketService.listen('new message', addNewMessage);
+
+            function sendMessage () {
+                if ($scope.message) {
+                    socketService.send('message send', $scope.message);
+                    $scope.message = '';
+                }
+            }
+
+            function addNewMessage (message) {
                 $scope.messages.push(message);
-            });
+                keepScrollInBottom();
+            }
 
-            $scope.sendMessage = function () {
-                socketService.send('message send', $scope.message);
-                $scope.message = '';
-            };
+            function keepScrollInBottom () {
+                var panelBodyElement = $element[0].getElementsByClassName('panel-body')[0];
+
+                $timeout(function () {
+                    panelBodyElement.scrollTop = panelBodyElement.scrollHeight - panelBodyElement.offsetHeight;
+                }, 50);
+            }
 
         }
 
